d087fec529c8e83de91202c51d0d93e59c3e2e05
Sean Yap <seanyckang@gmail.com>
2024-03-04 20:58:16 +0800

    feat: Added endpoint to upload study resource and thumbnail with resize function while maintaining aspect ratio, both thumbnail and resource uploaded to s3 bucket

--- StudyResource/models.py
+++ StudyResource/models.py
@@ -1,24 +1,21 @@
-from app import db
 from uuid import uuid4
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class StudyResource(db.Model):
     __tablename__ = "studyresource"
     uuid = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid4)
-    resourceID = db.Column(db.BIGINT, unique=True, nullable=False)
+    resourceID = db.Column(db.UUID(as_uuid=True), nullable=False, default=uuid4)
     resourceName = db.Column(db.String(255), nullable=False)  
+    resourceDesc = db.Column(db.String(255), nullable=False)
     resourcePrice = db.Column(db.Numeric(precision=10, scale=2))
     resources3URL = db.Column(db.String(255), nullable=False)
+    resourceThumbnailURL = db.Column(db.String(255), nullable=False)
     resourceLevel = db.Column(db.String(255), nullable=False)    
     sellerID = db.Column(db.Integer, nullable=False)
     sellerName = db.Column(db.String(255), unique=True, nullable=False)    
     
 
     def __repr__(self):
-         return f"<StudyResource(
-                uuid={self.uuid}, 
-                resourceID={self.resourceID}, 
-                resourceName={self.resourceName}, 
-                resourcePrice={self.resourcePrice}, 
-                resources3URL={self.resources3URL}, 
-                sellerID={self.sellerID}, 
-                sellerName={self.sellerName})>"
+         return f"<StudyResource(uuid={self.uuid}, resourceID={self.resourceID}, resourceName={self.resourceName}, resourcePrice={self.resourcePrice}, resources3URL={self.resources3URL}, sellerID={self.sellerID}, sellerName={self.sellerName})>"
