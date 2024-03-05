97d5be556bae1c0c729c71c12aaff7785f6266bf
Sean Yap <seanyckang@gmail.com>
2024-03-05 22:12:36 +0800

    feat: Added not found ui notification when level has no resources

--- ui/src/pages/Marketplace.tsx
+++ ui/src/pages/Marketplace.tsx
@@ -62,8 +62,7 @@ const Marketplace = () => {
         setNoData(false)       
       })
       .catch(error => {
-          console.error('Error fetching data:', error);
-          //TODO SET UP SHOW NO RESOURCES
+          console.error('Error fetching data:', error);          
           setPrimaryData([])
           setNoData(true)         
       });
@@ -87,8 +86,20 @@ const Marketplace = () => {
       </section>
 
       {/* Content portion */}
-      <section className='justify-center items-center grid sm:grid-cols-4 sm:gap-4 mx-20'>          
-          {noData && <p>No available resources at the level</p>}  
+      {noData &&
+      <section className='flex justify-center items-center'>
+        <div id="toast-danger" className="flex items-center max-w-md p-4 mb-4 text-gray-500 rounded-lg shadow" role="alert">
+          <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg">
+              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
+              </svg>
+              <span className="sr-only">Error icon</span>
+          </div>
+          <div className="ms-3 text-sm font-normal">No resources for sale at this level at this moment.</div>          
+        </div>        
+      </section>     
+      }  
+      <section className='justify-center items-center grid sm:grid-cols-4 sm:gap-4 mx-20'>           
           {primaryData.map((item, index) => {
             return (
               <ItemCard key={index} imgSrc={item.resourceThumbnailURL} itemName={item.resourceName} itemPrice={item.resourcePrice}/>
