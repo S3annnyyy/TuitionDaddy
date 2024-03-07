dd310bda16e41b70e266da6194a040348220e012
viqeey <fsantoso.2022@scis.smu.edu.sg>
2024-03-07 09:41:43 +0800

    chore: Renamed folder and removed body-parser pkg

--- Quiz/src/app.js
+++ Quiz/src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import routesHandler from "./routes/index.js";
-import bodyParser from "body-parser";
 import cors from "cors";
 import db from "./libs/db.js";
 
@@ -9,7 +8,7 @@ function startServer(port) {
 
     // App configs or middleware setups here
     app.use(cors());
-    app.use(bodyParser.json());
+    app.use(express.json());
 
     routesHandler.setupRoutes(app);
 
