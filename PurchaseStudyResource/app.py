35c9e3d3ed317cb6ea0a06cef11a22efc21a7e09
Sean Yap <seanyckang@gmail.com>
2024-03-11 13:57:21 +0800

    feat: Configured complex MS purchase study resource with user and studyresource MS. Pending payment and AMPQ integration

--- PurchaseStudyResource/app.py
+++ PurchaseStudyResource/app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
-import json
+from invokes import invoke_http 
 
 load_dotenv()
 
@@ -22,16 +22,40 @@ def purchase_study_resource():
     try:
         # Check if data parsed are in JSON format    
         if request.is_json:
-            data = request.get_json()           
+            data = request.get_json()
+            result = processResourcePurchase(data['resourceID'], data['sellerID'], data['buyerID'], data['buyerToken'], data['buyerPaymentDetails'])    
             return jsonResponse(200, message=f"Study resource ID: {data['resourceID']} purchased!")
         else:
             return jsonResponse(400, message="Invalid JSON input")
     except Exception as e:
-        # handle error
         return jsonResponse(500, message=f"Purchase study resource internal error: {e}")
     
-def processResourcePurchase():
-    raise NotImplementedError
+def processResourcePurchase(resourceID: int, sellerID: int, buyerID: int, buyerToken: str, buyerPaymentDetails: dict) -> None:
+    """
+    Process a resource purchase transaction.
+
+    Args:
+        resourceID (int): The ID of the resource being purchased.
+        sellerID (int): The ID of the seller.
+        buyerID (int): The ID of the buyer.
+        buyerPaymentDetails (dict): A dictionary containing payment details provided by the buyer.
+    """
+    print(resourceID, buyerID, sellerID, buyerPaymentDetails)
+
+    print("\n --------INVOKING user microservice--------")
+    user_result = invoke_http(userURL, method="GET", cookies={'Authorization': buyerToken})    
+    print(user_result)
+
+    print("\n --------INVOKING payment microservice--------")
+    # TODO
+
+    print("\n --------INVOKING studyresource microservice--------")
+    resource_result = invoke_http(f"{studyresourceURL}/{resourceID}", method="GET")
+    resource = resource_result['data']['resources3URL']
+    print(f"resource: {resource} obtained")
+
+    print("\n --------INVOKING Error & Notif microservice via AMPQ--------")
+    # TODO 
 
 if __name__ == "__main__":
     print(f"This is flask complex microservice {os.path.basename(__file__)} for purchasing study resource")          
